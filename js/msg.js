fb7397315daeac925bd13d3ec94420dbe67fd581
kc101010 <blueboi.kc@gmail.com>
2020-10-07 14:56:57 +0100

    Small JS addition

---
+++ js/msg.js
@@ -0,0 +1,7 @@
+function checkAlert(){
+    var alerted = localStorage.getItem('alerted') || '';
+    if(alerted != 'yes'){
+        alert("For the best experience it is advised that, on some Operating Systems, you view this webpage in full-screen mode. Please press F11 to do so, press again to exit fullscreen mode");
+        localStorage.setItem('alerted', 'yes');
+    }
+}
